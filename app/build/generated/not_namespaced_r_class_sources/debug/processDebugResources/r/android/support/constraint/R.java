b01b2ab84b9233b12a7fd1075cff11d23c0d3dc6
dangquang9a <dangquang9a@users.noreply.github.com>
2019-01-13 12:20:45 +0700

    Calendar layout

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080028;
-        public static final int end = 0x7f080053;
-        public static final int gone = 0x7f080062;
-        public static final int invisible = 0x7f08007b;
-        public static final int left = 0x7f080080;
-        public static final int packed = 0x7f08009a;
-        public static final int parent = 0x7f08009c;
-        public static final int percent = 0x7f08009f;
-        public static final int right = 0x7f0800a6;
-        public static final int spread = 0x7f0800c9;
-        public static final int spread_inside = 0x7f0800ca;
-        public static final int start = 0x7f0800cf;
-        public static final int top = 0x7f0800ea;
-        public static final int wrap = 0x7f0800fa;
+        public static final int end = 0x7f080054;
+        public static final int gone = 0x7f080063;
+        public static final int invisible = 0x7f080081;
+        public static final int left = 0x7f080086;
+        public static final int packed = 0x7f0800a0;
+        public static final int parent = 0x7f0800a2;
+        public static final int percent = 0x7f0800a5;
+        public static final int right = 0x7f0800ac;
+        public static final int spread = 0x7f0800cf;
+        public static final int spread_inside = 0x7f0800d0;
+        public static final int start = 0x7f0800d5;
+        public static final int top = 0x7f0800f1;
+        public static final int wrap = 0x7f080101;
     }
     public static final class styleable {
         private styleable() {}
--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
@@ -66,11 +66,11 @@ public final class R {
         public static final int adjust_width = 0x7f08001f;
         public static final int auto = 0x7f080024;
         public static final int dark = 0x7f080044;
-        public static final int icon_only = 0x7f080069;
-        public static final int light = 0x7f080081;
-        public static final int none = 0x7f080090;
-        public static final int standard = 0x7f0800ce;
-        public static final int wide = 0x7f0800f8;
+        public static final int icon_only = 0x7f08006a;
+        public static final int light = 0x7f080087;
+        public static final int none = 0x7f080096;
+        public static final int standard = 0x7f0800d4;
+        public static final int wide = 0x7f0800ff;
     }
     public static final class string {
         private string() {}
