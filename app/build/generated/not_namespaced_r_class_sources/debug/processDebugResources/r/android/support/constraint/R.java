a37e36b0870c4c4105e0f1fedd0656511ae7a1b9
dangquang9a <dangquang9a@gmail.com>
2019-01-14 23:11:45 +0700

    Layout

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -76,17 +76,17 @@ public final class R {
         public static final int bottom = 0x7f080028;
         public static final int end = 0x7f080055;
         public static final int gone = 0x7f080064;
-        public static final int invisible = 0x7f080088;
-        public static final int left = 0x7f08008d;
-        public static final int packed = 0x7f0800a7;
-        public static final int parent = 0x7f0800a9;
-        public static final int percent = 0x7f0800ac;
-        public static final int right = 0x7f0800b3;
-        public static final int spread = 0x7f0800d7;
-        public static final int spread_inside = 0x7f0800d8;
-        public static final int start = 0x7f0800dd;
-        public static final int top = 0x7f0800f5;
-        public static final int wrap = 0x7f080105;
+        public static final int invisible = 0x7f080087;
+        public static final int left = 0x7f08008c;
+        public static final int packed = 0x7f0800a6;
+        public static final int parent = 0x7f0800a8;
+        public static final int percent = 0x7f0800ab;
+        public static final int right = 0x7f0800b2;
+        public static final int spread = 0x7f0800d6;
+        public static final int spread_inside = 0x7f0800d7;
+        public static final int start = 0x7f0800dc;
+        public static final int top = 0x7f0800f4;
+        public static final int wrap = 0x7f080104;
     }
     public static final class styleable {
         private styleable() {}
--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/v4/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/v4/R.java
@@ -96,21 +96,21 @@ public final class R {
         public static final int icon = 0x7f080069;
         public static final int icon_group = 0x7f08006a;
         public static final int info = 0x7f080082;
-        public static final int italic = 0x7f080089;
-        public static final int line1 = 0x7f08008f;
-        public static final int line3 = 0x7f080090;
-        public static final int media_actions = 0x7f080094;
-        public static final int normal = 0x7f08009e;
-        public static final int notification_background = 0x7f08009f;
-        public static final int notification_main_column = 0x7f0800a0;
-        public static final int notification_main_column_container = 0x7f0800a1;
-        public static final int right_icon = 0x7f0800b4;
-        public static final int right_side = 0x7f0800b5;
-        public static final int status_bar_latest_event_content = 0x7f0800de;
-        public static final int text = 0x7f0800e6;
-        public static final int text2 = 0x7f0800e7;
-        public static final int time = 0x7f0800f1;
-        public static final int title = 0x7f0800f2;
+        public static final int italic = 0x7f080088;
+        public static final int line1 = 0x7f08008e;
+        public static final int line3 = 0x7f08008f;
+        public static final int media_actions = 0x7f080093;
+        public static final int normal = 0x7f08009d;
+        public static final int notification_background = 0x7f08009e;
+        public static final int notification_main_column = 0x7f08009f;
+        public static final int notification_main_column_container = 0x7f0800a0;
+        public static final int right_icon = 0x7f0800b3;
+        public static final int right_side = 0x7f0800b4;
+        public static final int status_bar_latest_event_content = 0x7f0800dd;
+        public static final int text = 0x7f0800e5;
+        public static final int text2 = 0x7f0800e6;
+        public static final int time = 0x7f0800f0;
+        public static final int title = 0x7f0800f1;
     }
     public static final class integer {
         private integer() {}
@@ -140,7 +140,7 @@ public final class R {
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0d0059;
+        public static final int status_bar_notification_info_overflow = 0x7f0d0064;
     }
     public static final class style {
         private style() {}
